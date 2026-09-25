7c4e868eb3c1720939501b6d8ae375dcf58104a1
Evsyukov Denis <denis@evsyukov.org>
2026-09-25 18:00:01 +0300

    chore(release): prepare version 0.1.1
    Increment the Android version for the F-Droid listing release and add its
    Fastlane changelog.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "org.evsyukov.shareding"
         minSdk = 28
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.1.0"
+        versionCode = 4
+        versionName = "0.1.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
